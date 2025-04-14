68b860f9730fe542424dab5ed49f0cb77036fece
Panxitus <gonzalezuribe.francisco@gmail.com>
2025-04-14 00:44:49 -0400

    SPA funcional

--- src/Components/Layouts/Section.jsx
+++ src/Components/Layouts/Section.jsx
@@ -1,9 +1,12 @@
 import vanguardia from '../../assets/img/vanguardia.jpg';
 import convenios from '../../assets/img/convenios.jpeg';
 import informate from '../../assets/img/informate.webp';
+import especialidades from '../../assets/img/Especialidades.jpg';
+import pacientes from '../../assets/img/pacientes.webp';
 const Section = () => {
   return (
     <section className='container_section'>
+        
         <div className='section_information'>
             <h3>Medicina de vanguardia</h3>
             <p>Los mejores equipamientos </p>
@@ -20,10 +23,15 @@ const Section = () => {
             <img src={convenios} alt="convenios"/>
         </div>
         <div className='section_information'>
-            <h3>Convenios y seguros </h3>
-            <p>Convenios abieros y cerrados </p>
-            <img src={convenios} alt="convenios"/>
-        </div>
+            <h3>Especialidades Medicas </h3>
+            <p>Las mejoras Especialidades </p>
+            <img src={especialidades} alt="convenios"/>
+        </div>  
+        <div className='section_information'>
+            <h3> Testimonios</h3>
+            <p> Nuestros pacientes</p>
+            <img src={pacientes} alt="convenios"/>
+        </div> 
     </section>
   )
 }
