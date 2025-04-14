013bca10f2bb1d89a3e710c00c17691e0471a995
FRANCISCO JAVIER GONZÁLEZ URIBE <fgonzalezu@intranet.ucsh.cl>
2025-04-14 17:17:03 -0400

    rama de actualización para incluir aspectos de responsibidad

---
+++ src/Components/Button/ButtonBurger.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ButtonBurger = () => {
+  return (
+    <div className='icon nav-icon-5'>
+        <span></span>
+        <span></span>
+        <span></span>
+
+    </div>
+  )
+}
+
+export default ButtonBurger
